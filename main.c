3352ee51203834492782b7bb4bd302f586acd332
pavellaminar <pavel.laminar@gmail.com>
2015-12-15 14:31:53 +0600

    Update main.c

--- main.c
+++ main.c
@@ -17,8 +17,8 @@ void timer_ini(){
   RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
   TIM6->ARR = AUTO_RELOAD;                      
   TIM6->PSC = PRESCALER;                     
-  TIM6->DIER = TIM_DIER_UIE;
-  TIM6->CR1 = TIM_CR1_UDIS;
+  TIM6->DIER |= TIM_DIER_UIE;
+  TIM6->CR1 |= TIM_CR1_UDIS;
   NVIC_EnableIRQ(TIM6_DAC_IRQn); 
   TIM6->CR1 |= TIM_CR1_CEN;       
 }
